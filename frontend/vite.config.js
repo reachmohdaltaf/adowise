e867ac3eb64f760793943b675aae184bfcd55909
reachmohdaltaf <reachmohdaltaf@gmail.com>
2025-05-17 16:05:36 +0530

    dddddd

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,6 +1,7 @@
 import path from "path"
 import tailwindcss from "@tailwindcss/vite"
 import react from "@vitejs/plugin-react"
+console.log('React plugin:', react);
 import { defineConfig } from "vite"
 
 // https://vite.dev/config/
