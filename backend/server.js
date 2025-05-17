702034ad09d4f2b0ed34d9f462272217bb526d48
reachmohdaltaf <reachmohdaltaf@gmail.com>
2025-05-17 19:52:05 +0530

    regex

--- backend/server.js
+++ backend/server.js
@@ -37,9 +37,9 @@ app.use('/api/v1/service', serviceRoutes)
 if (process.env.NODE_ENV === "production") {
 	app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-	app.get("*", (req, res) => {
-		res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
-	});
+	app.get(/^\/(?!api).*/, (req, res) => {
+  res.sendFile(path.resolve(__dirname, "../frontend/dist", "index.html"));
+});
 }
 
 
