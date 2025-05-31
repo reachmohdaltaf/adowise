4a804065e4cba4a19c19b2d28fa243363ddb5b81
reachmohdaltaf <reachmohdaltaf@gmail.com>
2025-05-31 15:42:31 +0530

    seo

--- frontend/src/seeker/components/layout/SeekerLayout.jsx
+++ frontend/src/seeker/components/layout/SeekerLayout.jsx
@@ -89,7 +89,7 @@ const SeekerLayout = () => {
 
       {/* Sliding Mobile Footer */}
       <div
-        className={`md:hidden fixed bottom-0 w-full transition-transform duration-500 ${
+        className={`md:hidden fixed bottom-0 z-20 w-full transition-transform duration-500 ${
           showFooter ? "translate-y-0" : "translate-y-full"
         }`}
       >
